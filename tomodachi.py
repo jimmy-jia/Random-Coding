9aadc17260bf308792e53dca552e1de02c29e928
jimmy-jia <jimmy.j.jia@gmail.com>
2014-09-25 04:51:55 -0500

    more tomodachi

--- tomodachi.py
+++ tomodachi.py
@@ -1,5 +1,5 @@
 import random
-import timedate
+import datetime
 
 
 def start():
@@ -7,22 +7,68 @@ def start():
 	print("I need food, water, and attention. I also need to sleep and poo!")
 	print("If you don't take good care of me, I'll die :(")
 	print("Type Help in order to view various inputs.")
-	global name = input("Now give me a name! \n")
+	global name
+	name = input("Now give me a name!: ")
+	main()
 
 def main():
-	userinput = input("What would you like to do?: ").lower()
+	global userinput
+	userinput = input("\nWhat would you like to do?: ").lower()
 	choice(userinput)
+
+
+def timecheck():
+	global hour
+	global minute
+	timenew = datetime.time
+	now = datetime.datetime.now()
+	hour = now.hour
+	minute = now.minute
+
 
 def choice(userinput):
 
-	if userinput = help:
+	if userinput == "help":
 		help()
-
+	if userinput == "timecheck":
+		timecheck()
+		print ("Current time is:", hour, ":",minute)
+		main()
+	if userinput == "name":
+		print ("Your pet's name is", name +",","how could you forget!")
+		main()
+	print("Sorry, I don't recognize that input, please try again!")
+	main()
 
 
 
 def help():
-	print("What would you like help with?")
-	print("1: Inputs, 2: Gameplay, 3: Credits")
-	helpchoice = input("Choice?: ").lower()
-	if helpchoice = 1 or helpchoice = "inputs" 
+	print("\nWhat would you like help with?")
+	print("1: Inputs, 2: Gameplay, 3: Credits, 4: Return to game")
+	helpchoice = str(input("Choice?: ")).lower()
+	if helpchoice == "1" or helpchoice == "inputs":
+		print("Inputs are:")
+		print("Timecheck = Current time")
+		print("Name = Your pet's name")
+		helpask()
+	elif helpchoice == "2" or helpchoice == "gameplay":
+		print("Play the game\n")
+		helpask()
+	elif helpchoice == "3" or helpchoice == "credits":
+		print("Created by Jimmy Jia\n")
+		helpask()
+	elif helpchoice == "4" or helpchoice == "return" or helpchoice =="return to game":
+		main()
+
+def helpask():
+	morehelp = input("Would you like more help? ").lower()
+	if morehelp == "yes":
+		help()
+	elif morehelp == "no":
+		main()
+	else:
+		print("Please type either yes or no.")
+		helpask()
+
+
+start()
