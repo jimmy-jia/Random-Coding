0d3aca5ffc28f169707968df5d7bcb16cd411922
jimmy-jia <jimmy.j.jia@gmail.com>
2014-09-25 19:34:51 -0500

    tomodachi and newtj

--- tomodachi.py
+++ tomodachi.py
@@ -10,7 +10,7 @@ def start():
 	global name
 	name = input("Now give me a name!: ")
 	global timeold
-	timeold = datetime.time
+	timeold = datetime.datetime.now()
 	main()
 
 def main():
@@ -22,15 +22,17 @@ def main():
 def timecheck():
 	global hour
 	global minute
-	global timenew
-	timenew = datetime.time
+	global now
 	now = datetime.datetime.now()
 	hour = now.hour
 	minute = now.minute
+	mytimedelta()
 
 def mytimedelta():
 	global timedif
-	timedif = datetime.timedelta(timeold)
+	timedif = now - timeold
+	global cdiff
+	cdiff = divmod(timedif.days*86400 + timedif.seconds, 60)
 
 def choice(userinput):
 
@@ -40,14 +42,20 @@ def choice(userinput):
 		timecheck()
 		print ("Current time is: "+str(hour)+":"+str(minute))
 		print ("The last time you checked on", name, "was at", str(hour)+":"+str(minute)+ ".")
+		print ("Time difference is", timedif.minutes, "minutes and", timedif.seconds, "seconds.")
 		main()
 	if userinput == "name":
 		print ("Your pet's name is", name +",","how could you forget!")
 		main()
+	if userinput == "sleep":
+		timecheck()
+		mytimedelta()
+	if userinput == "hunger"
+		hunger()
 	if userinput == "1":
 		mytimedelta()
 		print(timedif)
-	print("Sorry, I don't recognize that input, please try again!")
+	print("Sorry, I don't recognize that input, please try again! \nIf you don't remember the inputs, please type help!")
 	main()
 
 
@@ -60,6 +68,7 @@ def help():
 		print("Inputs are:")
 		print("Timecheck = Current time")
 		print("Name = Your pet's name")
+		print("Hunger = Your current pet hunger")
 		helpask()
 	elif helpchoice == "2" or helpchoice == "gameplay":
 		print("Play the game\n")
@@ -79,6 +88,21 @@ def helpask():
 	else:
 		print("Please type either yes or no.")
 		helpask()
+
+def hunger():
+
+	if fed <25:
+		print("Your pet is nearly dead! You need to feed him!")
+	elif fed <50:
+		print("Your pet is hungry! You should feed him!")
+	elif fed <100:
+		print("Your pet is ok! He is satisfied with his tumtum!")
+	elif fed =100:
+		print("Wow you just fed him! Great job!")
+	elif fed >100:
+		print("You overfed him :( be careful!")
+	main()
+
 
 
 start()
